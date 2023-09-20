deb42f9de66692b9752106046f9fac251fbd9398
jamuna <jamuna2612@gmail.com>
2023-09-20 16:52:45 -0700

    Added the new functionality

--- src/test/java/org/example/stepDefinitions/StepDefinitionImpl.java
+++ src/test/java/org/example/stepDefinitions/StepDefinitionImpl.java
@@ -1,4 +1,6 @@
 package org.example.stepDefinitions;
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -18,7 +20,7 @@ public class StepDefinitionImpl extends BaseTest {
     public IntroPage introPage;
 
     @Given("User is in login page")
-    public void  User_is_in_login_page() throws IOException, InterruptedException {
+    public void User_is_in_login_page() throws IOException, InterruptedException {
 
         driver = initializeDriver();
         WelcomePage welcomePage = new WelcomePage(driver);
@@ -30,17 +32,49 @@ public class StepDefinitionImpl extends BaseTest {
     @Given("^Logged in with username (.+) and password(.+)$")
     public void Logged_in_with_username_and_password(String userName, String password) throws InterruptedException {
 
-        loginPage.loginDataClick(userName,password);
+        loginPage.loginDataClick(userName, password);
         Thread.sleep(1000);
     }
+
     @When("User click login")
-    public void User_click_login(){
+    public void User_click_login() {
         loginPage.submit();
     }
+
     @Then("{string} message is displayed")
     public void message_is_displayed(String string) {
         String actualLoginMessage = loginPage.errorMsg();
         Assert.assertTrue(actualLoginMessage.equalsIgnoreCase(string));
         driver.close();
+    }
+
+    @Given("User is in intro page")
+    public void user_is_in_intro_page() throws IOException, InterruptedException {
+        driver = initializeDriver();
+        WelcomePage welcomePage = new WelcomePage(driver);
+        welcomePage.click();
+        loginPage = welcomePage.login();
+        loginPage.loginDataClick("james", "apple@123");
+        introPage = loginPage.submit();
+    }
+
+    @When("user click linkedList")
+    public void user_click_linked_list() {
+        introPage.llClick();
+    }
+
+    @Then("click intro link")
+    public void click_intro_link() {
+        introPage.introClick();
+    }
+
+    @Then("navigate back to intro page")
+    public void navigate_back_to_intro_page() {
+        navigateBack(2);
+    }
+
+    @And("click tree link")
+    public void clickTreeLink() {
+        introPage.treeClick();
     }
 }
