066f06a4921333facc22b4f4bc3d7deff6251881
jamuna <jamuna2612@gmail.com>
2023-09-27 09:06:05 -0700

    Revert "Merge pull request #1 from Jamuna2612/apeksha_sdet127_dev"
    This reverts commit e6f788a1445333887d7602ed03c1d68edc2ab2b5, reversing
    changes made to e4f5bf8a0e839ee9c57c21683439a6e3ccf582a2.

--- src/main/java/org/example/testsObjects/pageObjects/IntroPage.java
+++ src/main/java/org/example/testsObjects/pageObjects/IntroPage.java
@@ -1,14 +1,10 @@
 package org.example.testsObjects.pageObjects;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class IntroPage {
 
@@ -40,10 +36,6 @@ public class IntroPage {
 
     @FindBy(css = "div[class*='alert-primary']")
     WebElement invalidDataMessage;
-    
-    @FindBy(css = "div[class*='alert-primary']")
-    WebElement alertDataMessage;
-    
 
     public void llClick() {
         linkedList.click();
@@ -76,26 +68,6 @@ public class IntroPage {
         String message = invalidDataMessage.getText();
         return message;
     }
-    
-    public String getAlertDataMessage() {
-    	return alertDataMessage.getText();
-    }
-    
-    public boolean waitForElementDisplayed(String elementStr) {
-    	WebElement element = null;
-    	if (elementStr == "datastructure") {
-    		element = dataStructure;
-    	}else if (elementStr == "alertDataMessage") {
-    		element = alertDataMessage;
-    	}
-    	// add more element else if conditions based on scenario
-    	
-	    WebElement testElement = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(element));
-	    if (testElement.isDisplayed()) {
-	      return true;
-	    }    	
-    	return false;
-    }    
 
 
 }
--- src/test/java/org/example/tests/RegistrationErrorValidationTest.java
+++ src/test/java/org/example/tests/RegistrationErrorValidationTest.java
@@ -5,143 +5,42 @@ import org.example.testsObjects.pageObjects.RegisterPage;
 import org.example.testsObjects.pageObjects.WelcomePage;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class RegistrationErrorValidationTest extends BaseTest {
 
-	WebDriver driver;
 
     @Test
-    public void numbersOnlyPasswordRegisterError() throws IOException, InterruptedException {
-        driver = initializeDriver();
+    public void registrationErrorValidationWithNumericValue() throws IOException, InterruptedException {
+
+
+        WebDriver driver = initializeDriver();
         WelcomePage welcomePage = new WelcomePage(driver);
-        
         welcomePage.click();
-        RegisterPage registerPage = welcomePage.registerClick();
-        
-        // username field displayed on webpage
-        if (registerPage.waitForElementDisplayed("username")) {
-        	// generate username with length of 6 chars
-        	registerPage.generateUsername(6, "alphabetsonly");
-        	
-        	// generate password
-        	registerPage.generatePassword(8, "numbers");
-        	
-        	registerPage.inputCredentials();
-            registerPage.submitClick();
-            String actualErrStr = registerPage.getErrorMessage();
-            System.out.println("Actual Error: " + actualErrStr);
-           // Assert.assertEquals(actualErrStr, "password_mismatch:The two password fields didn’t match.");
-        }
-        driver.close();
+        RegisterPage loginPage = welcomePage.registerClick();
+        loginPage.login("geng", "6534", "6534");
+        loginPage.submitClick();
+        String actual = loginPage.errorMessage();
+        Assert.assertEquals(actual, "password_mismatch:The two password fields didn’t match.");
     }
 
     @Test
-    public void passwordMismatchRegisterError() throws IOException, InterruptedException {
-        driver = initializeDriver();
-        WelcomePage welcomePage = new WelcomePage(driver);
-        
-        welcomePage.click();
-        RegisterPage registerPage = welcomePage.registerClick();
-        
-        // username field displayed on web page
-        if (registerPage.waitForElementDisplayed("username")) {
-        	// generate username with length of 6 chars
-        	registerPage.generateUsername(6, "alphabetsOnly");
-        	
-        	// generate password
-        	registerPage.generatePassword(8, "passwordmismatch");
-        	
-        	registerPage.inputCredentials();
-            registerPage.submitClick();
-            String actualErrStr = registerPage.getErrorMessage();
-            System.out.println("Actual Error: " + actualErrStr);
-           // Assert.assertEquals(actualErrStr, "password_mismatch:The two password fields didn’t match.");
-        }
-        driver.close();
-    }
-    
-    @Test
-    public void longUserNameRegisterError() throws IOException, InterruptedException {
-        driver = initializeDriver();
-        WelcomePage welcomePage = new WelcomePage(driver);
-        
-        welcomePage.click();
-        RegisterPage registerPage = welcomePage.registerClick();
-        
-        // username field displayed on web page
-        if (registerPage.waitForElementDisplayed("username")) {
-        	// generate username with length of 6 chars
-        	registerPage.generateUsername(151, "alphabetsOnly");
-        	
-        	// generate password
-        	registerPage.generatePassword(8, "alphanumbericSpecialChars");
-        	
+    public void registrationErrorWithAlphaNumeric() throws IOException, InterruptedException {
 
-        	registerPage.inputCredentials();
-            registerPage.submitClick();
-            String actualErrStr = registerPage.getErrorMessage();
-            System.out.println("Actual Error: " + actualErrStr);
-            // password_mismatch:The two password fields didn�t  match,
-            // Here didn't sometimes comes as ', sometimes ? and sometimes symbol
-            // hence assert not equals "" is used
-           // Assert.assertNotEquals(actualErrStr, "");
-        }
-        driver.close();
-    }    
-    
-    @Test
-    public void invalidSpecialCharUsernameRegisterError() throws IOException, InterruptedException {
-        driver = initializeDriver();
+
+        WebDriver driver = initializeDriver();
         WelcomePage welcomePage = new WelcomePage(driver);
-        
         welcomePage.click();
-        RegisterPage registerPage = welcomePage.registerClick();
-        
-        // username field displayed on web page
-        if (registerPage.waitForElementDisplayed("username")) {
-        	// generate username with length of 6 chars
-        	registerPage.generateUsername(4, "invalidspecialchars");
-        	
-        	// generate password
-        	registerPage.generatePassword(8, "alphanumbericSpecialChars");
-        	
-        	registerPage.inputCredentials();
-            registerPage.submitClick();
-            String actualErrStr = registerPage.getErrorMessage();
-            System.out.println("Actual Error: " + actualErrStr);
-          //  Assert.assertNotEquals(actualErrStr, "");
-        }
-        driver.close();
+        RegisterPage loginPage = welcomePage.registerClick();
+        loginPage.login("Johan", "!6534@", "!6534@");
+        loginPage.submitClick();
+        String actual = loginPage.errorMessage();
+        Assert.assertEquals(actual, "password_mismatch:The two password fields didn’t match.");
     }
-    
-    @Test
-    public void shortPasswordRegisterError() throws IOException, InterruptedException {
-        driver = initializeDriver();
-        WelcomePage welcomePage = new WelcomePage(driver);
-        
-        welcomePage.click();
-        RegisterPage registerPage = welcomePage.registerClick();
-        
-        // username field displayed on web page
-        if (registerPage.waitForElementDisplayed("username")) {
-        	// generate username with length of 6 chars
-        	registerPage.generateUsername(4, "alphabetsonly");
-        	
-        	// generate password
-        	registerPage.generatePassword(5, "alphanumbericSpecialChars");
-        	
-        	registerPage.inputCredentials();
-            registerPage.submitClick();
-            String actualErrStr = registerPage.getErrorMessage();
-            System.out.println("Actual Error: " + actualErrStr);
-          //  Assert.assertNotEquals(actualErrStr, "");
-        }
-        driver.close();
-    }   
+
+
 }
 
 
